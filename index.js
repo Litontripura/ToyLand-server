34cdaf7ab2b38cc3229c60bb197440facd2f50b9
Litontripura <litontripura01@gmail.com>
2023-05-20 13:36:16 +0600

    i have update the vercel config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
     const toyCollection = client.db('toyCollectionDb').collection('toys')
     const toyPhotoCollection = client.db('toyCollectionDb').collection('toyGalary')
    
@@ -50,6 +50,12 @@ async function run() {
     app.get('/toys/:email', async(req,res)=>{
       console.log(req.params.email);
       const result = await toyCollection.find({sellerEmail:req.params.email}).toArray()
+      res.send(result)
+    })
+    app.get('/singletoy/:id',async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.findOne(query)
       res.send(result)
     })
 
